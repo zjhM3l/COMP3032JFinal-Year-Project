59ecc9e16066ce38c281362b1953591d95d857f6
Grey Xiao <zhiyou.xiao@ucdconnect.ie>
2024-04-07 14:21:31 +0800

    upload Happy emotion and neutral emotion responses text

--- app/static/js/emotion_analysis_text_response.js
+++ app/static/js/emotion_analysis_text_response.js
@@ -2,7 +2,7 @@ var emotion_label = document.querySelector('#Tree-hole-AI-response-emotion');
 var emotion_response_text_label = document.querySelector('#Tree-hole-AI-response-text');
 var emotion_saying_text_label = document.querySelector('#Tree-hole-AI-famous-saying');
 
-var emotion_analysis_result = 'Neutral';
+var emotion_analysis_result = 'Happy';
 var emotion_classes = [
     'Neutral',
     'Happy',
@@ -17,6 +17,8 @@ if (emotion_analysis_result == 'Neutral'){
     emotion_saying_text_label = '"True happiness is to enjoy the present, without anxious dependence upon the future, not to amuse ourselves with either hopes or fears but to rest satisfied with what we have, which is sufficient, for he that is so wants nothing. The greatest blessings of mankind are within us and within our reach. A wise man is content with his lot, whatever it may be, without wishing for what he has not." -- Stoic philosopher Seneca ';
 }else if (emotion_analysis_result == 'Happy'){
     emotion_label.textContent = 'Happy and light mood';
+    emotion_response_text_label.textContent = 'The Happy emotion is a beautiful and uplifting experience that brightens our days and fills our hearts with joy. It allows us to appreciate the beauty of life, cherish moments of connection with others, and find contentment in the simplest of pleasures. Happiness fosters resilience, strengthens relationships, and enhances overall well-being. It\'s a beacon of positivity that radiates warmth and optimism, inspiring us to embrace each day with gratitude and enthusiasm.';
+    emotion_saying_text_label.textContent = '"Happiness is a butterfly, which when pursued, is always just beyond your grasp, but if you sit down quietly, may alight upon you." - Nathaniel Hawthorne';
 
 }else if (emotion_analysis_result == 'Angry'){
     emotion_label.textContent = 'Rushing and unstable mood';
