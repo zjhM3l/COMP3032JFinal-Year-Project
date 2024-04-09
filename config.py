fc400d58f7b48d1c9267e44396cda709ff753544
Feihe-Huang <fh1104028870@gmail.com>
2024-04-09 15:30:38 +0800

    Pagination Feihe Huang

--- config.py
+++ config.py
@@ -22,6 +22,7 @@ class Config:
     FLASKY_FOLLOWERS_PER_PAGE = 50
     FLASK_ANNOUNCEMENT_PER_PAGE = 2
     FLASKY_COMMENTS_PER_PAGE = 5
+    POST_BLOG_PER_PAGE = 4
     UPLOAD_FOLDER = os.getcwd() + '/app/static/avatars/'
     UPLOAD_EPOST = os.getcwd() + '/app/static/postPhoto/'
     FLASK_COMMENTS_PER_PAGE = 20
